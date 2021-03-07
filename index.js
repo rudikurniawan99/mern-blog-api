7610cb04abf27851ec9d298e7bdea0bb15c7cf2e
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-03-07 14:29:45 +0700

    solving CORS Origin error

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const port = 4000
 const productsRoutes = require('./src/routes/products')
 
 
+/**
+ * How to solve CORS ORIGIN error
+ */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+  res.setHeader('Access-Allow-Control-Header', 'Content-Type, Authorization')
+  next()
+})
+/**------------------------------ */
+
+
 //use products routes
 app.use('/', productsRoutes)
 
